a06dd73ef269776d3f5d339f944a964dceb03ad6
Irfan Fauzi <irfanfauziyk@gmail.com>
2026-04-21 10:11:25 +0700

    membuat user repository

---
+++ src/main/java/com/example/praktikum7/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.praktikum7.repository;
+
+import com.example.praktikum7.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
